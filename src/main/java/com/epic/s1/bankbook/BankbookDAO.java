c785454214b49a20516394c92811ee495a9bfb37
JulyRam <wogjs715@naver.com>
2021-08-31 11:38:48 +0900

    7

---
+++ src/main/java/com/epic/s1/bankbook/BankbookDAO.java
@@ -0,0 +1,118 @@
+package com.epic.s1.bankbook;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.epic.s1.util.DBConnector;
+
+public class BankbookDAO {
+	
+	private DBConnector dbConnector;
+	
+	public BankbookDAO() {
+		dbConnector = new DBConnector();
+	}
+	
+	//setInsert
+	public int setInsert(BankBookDTO bankBookDTO) {
+		Connection con = dbConnector.getConnect();
+		PreparedStatement st= null;
+		int result=0;
+		String sql ="insert into bankbook (bookNumber, bookName, bookRate, bookSale) "
+				+ "values (bankbook_seq.nextval, ?, ?, ?)";
+		
+		try {
+			st = con.prepareStatement(sql);
+			st.setString(1, bankBookDTO.getBookName());
+			st.setDouble(2, bankBookDTO.getBookRate());
+			st.setInt(3, bankBookDTO.getBookSale());
+			
+			result = st.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			dbConnector.disConnect(st, con);
+		}
+		
+		return result;
+		
+	}
+	
+	
+	
+	//getList
+	public ArrayList<BankbookDTO> getList() {
+		Connection con = dbConnector.getConnect();
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		ArrayList<BankbookDTO> ar = new ArrayList<BankbookDTO>();
+		
+		String sql= "select * from bankbook";
+		
+		try {
+			st = con.prepareStatement(sql);
+			
+			rs = st.executeQuery();
+			
+			while(rs.next()) {
+				BankbookDTO bankBookDTO = new BankbookDTO();
+				bankBookDTO.setBookNumber(rs.getLong("bookNumber"));
+				bankBookDTO.setBookName(rs.getString("bookName"));
+				bankBookDTO.setBookRate(rs.getDouble("bookRate"));
+				bankBookDTO.setBookSale(rs.getInt("bookSale"));
+				ar.add(bankBookDTO);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			dbConnector.disConnect(rs, st, con);
+		}
+		
+		return ar;
+		
+	}
+	
+	public BankbookDTO getSelect(BankbookDTO bankBookDTO) {
+		
+		Connection con = dbConnector.getConnect();
+		System.out.println("Con : "+con);
+		PreparedStatement st=null;
+		ResultSet rs = null;
+		BankbookDTO result=null;
+		String sql ="SELECT * FROM BANKBOOK WHERE BOOKNUMBER=?";
+		
+		try {
+			st = con.prepareStatement(sql);
+			
+			st.setLong(1, bankBookDTO.getBookNumber());
+			
+			rs = st.executeQuery();
+			
+			if(rs.next()) {
+				result = new BankbookDTO();
+				result.setBookNumber(rs.getLong("bookNumber"));
+				result.setBookName(rs.getString("bookName"));
+				result.setBookRate(rs.getDouble("bookRate"));
+				result.setBookSale(rs.getInt("bookSale"));
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			dbConnector.disConnect(rs, st, con);
+		}
+		
+		return result;
+		
+	}
+	
+
+}
---
+++ src/main/java/com/epic/s1/bankbook/bankbookController.java
@@ -0,0 +1,48 @@
+package com.epic.s1.bankbook;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/bankbook/*")
+public class bankbookController {
+	//pojo (plain Old Java Object)
+	
+	@RequestMapping(value =  "bankbookList.do", method = RequestMethod.GET)
+	public ModelAndView list (Integer [] num) {
+		for (Integer i : num) {
+			System.out.println(i);
+		}
+		System.out.println("bankbook list");
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("bnakbook/bankbookList");
+		
+		return mv;
+	}
+	
+	@RequestMapping("bankbookSelect")
+	public String select(@RequestParam(defaultValue = "1", value = "n") Integer num, String name, Model model) {
+		System.out.println("Value: "+num);
+		System.out.println("Name : "+name);
+		BankbookDTO bankbookDTO = new BankbookDTO();
+		bankbookDTO.setBookName("BookName");
+		model.addAttribute("dto", bankbookDTO);
+		model.addAttribute("test", "iu");
+		return "bankbook/bankbookSelect";
+	}
+	
+	@RequestMapping("bankbookInsert.do")
+	public String insert(BankbookDTO bankbookDTO) {
+		System.out.println(bankbookDTO.getBookName());
+		System.out.println("insert");
+		return "redirect:../";
+	}
+	
+}
